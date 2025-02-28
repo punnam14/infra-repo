324deaa61ebe669b17f3e5b6bc58c467158e549f
P.S <1998punnam@gmail.com>
2025-02-28 11:39:21 -0800

    Add ECR to workflow

--- setup.sh
+++ setup.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# Update system packages
 sudo yum update -y
 
-# Install required dependencies
-sudo yum install -y git python3 python3-pip
+sudo yum install -y git python3 python3-pip docker
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ec2-user
 
 sudo ln -s /usr/bin/python3 /usr/bin/python
-
-# Install Python dependencies
 sudo python3 -m pip install --upgrade pip
 
 cd /home/ec2-user
@@ -17,16 +16,10 @@ cd hottake/backend
 
 sudo python3 -m pip install -r requirements.txt
 sudo python3 -m pip install pytest
-
-# Ensure correct ownership & permissions
 sudo chown -R ec2-user:ec2-user /home/ec2-user/hottake/backend
 sudo chmod -R 775 /home/ec2-user/hottake/backend
-
-# Ensure SQLite test database exists with correct permissions
 sudo touch test.db
 sudo chown ec2-user:ec2-user test.db
 sudo chmod 666 test.db
-
-# Ensure pytest cache directory has correct permissions
 sudo mkdir -p .pytest_cache
 sudo chmod -R 777 .pytest_cache
