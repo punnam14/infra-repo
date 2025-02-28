a58d7b01f9e6df93176fe028e31e94091e31dab9
P.S <1998punnam@gmail.com>
2025-02-27 22:14:53 -0800

    Checked backend dependencies

--- setup.sh
+++ setup.sh
@@ -10,4 +10,11 @@ sudo ln -s /usr/bin/python3 /usr/bin/python
 
 # Install Python dependencies
 sudo python3 -m pip install --upgrade pip
+
+cd /home/ec2-user
+git clone https://github.com/punnam14/hottake.git
+cd hottake/backend
+
 sudo python3 -m pip install pytest
+
+sudo python3 -m pip install -r requirements.txt
