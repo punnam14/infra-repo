28fd9ce1bfc88d6c20d595e94d640c13b4a3dcbe
P.S <1998punnam@gmail.com>
2025-02-27 22:00:03 -0800

    Debug test pass/fail

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ sudo yum update -y
 # Install required dependencies
 sudo yum install -y git python3 python3-pip
 
+sudo ln -s /usr/bin/python3 /usr/bin/python
+
 # Install Python dependencies
 sudo python3 -m pip install --upgrade pip
 sudo python3 -m pip install pytest
