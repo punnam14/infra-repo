17c05b718ecd7d8b918c4fe95b9d4acb8a8d2441
P.S <1998punnam@gmail.com>
2025-02-27 21:44:32 -0800

    Test workflow without docker

--- setup.sh
+++ setup.sh
@@ -4,19 +4,7 @@
 sudo yum update -y
 
 # Install required dependencies
-sudo yum install -y git docker python3 python3-pip
-
-# Install Docker Compose (manually for Amazon Linux 2023)
-sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-
-# Start and enable Docker service
-sudo systemctl start docker
-sudo systemctl enable docker
-
-# Add ec2-user to the Docker group (to allow running Docker without sudo)
-sudo usermod -aG docker ec2-user
+sudo yum install -y git python3 python3-pip
 
 # Install Python dependencies
 sudo python3 -m pip install --upgrade pip
